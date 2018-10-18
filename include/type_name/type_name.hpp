6fdd9ab60fb7f811aa1106006a45b9c6a355497e
Adam Badura <adam.f.badura@gmail.com>
2018-10-19 01:44:55 +0200

    Add GCC 8.1 improved version
    GCC 8.1 (over GCC 7.3) allows to make the get() function variables
    also constexpr.
    
    Furthermore, assert is now replace with static_assert, which is
    possible due to those variables being constexpr. (And it seems this
    is the only benefit of it.)

--- include/type_name/type_name.hpp
+++ include/type_name/type_name.hpp
@@ -2,7 +2,9 @@
 #define ADAM_BADURA_TYPE_NAME_TYPE_NAME_HPP
 
 #if defined(__GNUC__)
-#	if (__GNUC__ > 7) || (__GNUC__ == 7 && __GNUC_MINOR__ >= 3)
+#	if (__GNUC__ > 8) || (__GNUC__ == 8 && __GNUC_MINOR__ >= 1)
+#		include "type_name.gcc.8.1.hpp"
+#	elif (__GNUC__ > 7) || (__GNUC__ == 7 && __GNUC_MINOR__ >= 3)
 #		include "type_name.gcc.7.3.hpp"
 #	else
 #		error When using GCC, at least version 7.3 is required.
