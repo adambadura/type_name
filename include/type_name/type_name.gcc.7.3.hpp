cb2e1ee30ab3ea440bd82aa06ddd22eb502c4534
Adam Badura <adam.f.badura@gmail.com>
2018-10-19 01:35:32 +0200

    Separate version selection from implementation
    The main header (type_name.hpp) only selects (based on preprocessor
    detection of the compiler) which specific header to include.
    
    This would result in some code repetition, however, overall it should
    be cleaner when supporting more versions.

---
+++ include/type_name/type_name.gcc.7.3.hpp
@@ -0,0 +1,38 @@
+#ifndef ADAM_BADURA_TYPE_NAME_TYPE_NAME_GCC_7_3_HPP
+#define ADAM_BADURA_TYPE_NAME_TYPE_NAME_GCC_7_3_HPP
+
+#include <cassert>
+#include <string_view>
+
+template<typename T>
+struct type_name
+{
+private:
+	static constexpr auto get() noexcept
+	{
+		const std::string_view full_name{ __PRETTY_FUNCTION__ };
+		const std::string_view left_marker{ "[with T = " };
+		const std::string_view right_marker{ "]" };
+
+		const auto left_marker_index = full_name.find(left_marker);
+		assert(left_marker_index != std::string_view::npos);
+		const auto start_index = full_name.find(left_marker) + left_marker.size();
+		const auto end_index = full_name.find(right_marker, left_marker_index);
+		assert(end_index != std::string_view::npos);
+		const auto length = end_index - start_index;
+
+		return full_name.substr(start_index, length);
+	}
+
+public:
+	using value_type = std::string_view;
+	static constexpr value_type value{ get() };
+
+	constexpr operator value_type() const noexcept { return value; }
+	constexpr value_type operator()() const noexcept { return value; }
+};
+
+template<typename T>
+inline constexpr auto type_name_v = type_name<T>::value;
+
+#endif
--- include/type_name/type_name.hpp
+++ include/type_name/type_name.hpp
@@ -1,48 +1,14 @@
 #ifndef ADAM_BADURA_TYPE_NAME_TYPE_NAME_HPP
 #define ADAM_BADURA_TYPE_NAME_TYPE_NAME_HPP
 
-#if defined(__GNUC__) && ((__GNUC__ > 7) || (__GNUC__ == 7 && __GNUC_MINOR__ >= 3))
-
-#include <cassert>
-#include <string_view>
-
-template<typename T>
-struct type_name
-{
-private:
-	static constexpr auto get() noexcept
-	{
-		const std::string_view full_name{ __PRETTY_FUNCTION__ };
-		const std::string_view left_marker{ "[with T = " };
-		const std::string_view right_marker{ "]" };
-
-		const auto left_marker_index = full_name.find(left_marker);
-		assert(left_marker_index != std::string_view::npos);
-		const auto start_index = full_name.find(left_marker) + left_marker.size();
-		const auto end_index = full_name.find(right_marker, left_marker_index);
-		assert(end_index != std::string_view::npos);
-		const auto length = end_index - start_index;
-
-		return full_name.substr(start_index, length);
-	}
-
-public:
-	using value_type = std::string_view;
-	static constexpr value_type value{ get() };
-
-	constexpr operator value_type() const noexcept { return value; }
-	constexpr value_type operator()() const noexcept { return value; }
-};
-
-template<typename T>
-inline constexpr auto type_name_v = type_name<T>::value;
-
-#else
-#	if defined(__GNUC__)
-#		error When using GCC, at least version 7.3 is required.
+#if defined(__GNUC__)
+#	if (__GNUC__ > 7) || (__GNUC__ == 7 && __GNUC_MINOR__ >= 3)
+#		include "type_name.gcc.7.3.hpp"
 #	else
-#		error Unsupported compiler
+#		error When using GCC, at least version 7.3 is required.
 #	endif
+#else
+#	error Unsupported compiler.
 #endif
 
 #endif
