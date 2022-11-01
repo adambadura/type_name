62423e33dc0ca15dfe1430efd31e02f3a627df4a
qqwertyui <p.warmke@wp.pl>
2022-11-01 21:16:34 +0100

    Fix gcc specific includes

--- include/type_name/type_name.msvc.hpp
+++ include/type_name/type_name.msvc.hpp
@@ -2,7 +2,7 @@
 #define ADAM_BADURA_TYPE_NAME_TYPE_NAME_MSVC_HPP
 
 #if (_MSC_VER >= 1921)
-#	include "type_name.msvc.19_21.hpp"
+#	include "type_name.msvc.19.21.hpp"
 #else
 #	error When using Microsoft (R) C/C++ Optimizing Compiler, at least version 19.21 is required.
 #endif
